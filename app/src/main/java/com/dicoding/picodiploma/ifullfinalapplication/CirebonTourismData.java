3260fb56680b4d20654988617b4d776fd1cdd074
syaiful <syaiful@bni.co.id>
2019-11-27 14:48:08 +0700

    first init

---
+++ app/src/main/java/com/dicoding/picodiploma/ifullfinalapplication/CirebonTourismData.java
@@ -0,0 +1,21 @@
+package com.dicoding.picodiploma.ifullfinalapplication;
+
+public class CirebonTourismData {
+    private static String[] cirebonTourismName = {
+            "Keraton Kesepuhan",
+            "Keraton Kanoman",
+            "Masjid Agung Sang Cipta Rasa",
+            "Makam Sunan Gunung Jati",
+            "Taman Sari Gua Sunyaragi",
+            "Cirebon Waterland Taman Ade Irma Suryani",
+            "Bukit Gronggong",
+            "Telaga Remis",
+            "Setu Patok",
+            "Sentra Batik Trusmi",
+            "Wanawisata Ciwaringin",
+            "Hutan Plangon",
+            "Desa Wisata Cikalahang",
+            "Pantai Kejawanan",
+            "Alun-alun Kejaksan"
+    }
+}
